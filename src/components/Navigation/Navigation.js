ef57fec39faf343484be16cde4066978db4a27c4
Casey Ferrara <caseyferrara@Caseys-MacBook-Pro.local>
2021-02-04 21:00:47 -0600

    Adding navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<div>
+			<p>Color Ahoy</p>
+			<header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+				<nav class="f6 fw6 ttu tracked">
+					<a class="link dim white dib mr3" href="#" title="Home">Sign Out</a>
+					<a class="link dim white dib mr3" href="#" title="About">Profile</a>
+				</nav>
+			</header>
+		</div>
+	)
+}
+
+export default Navigation;
